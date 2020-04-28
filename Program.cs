a807cacc957111f18fdcac6b8fbfe21d58d25321
Terrence Kunstek <terrence@kunstek.com>
2020-04-28 13:24:56 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SqlClient;
+using S2.BlackSwan.SupplyCollector.Ado;
+using S2.BlackSwan.SupplyCollector.Models;
+
+namespace S2DbTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Expecting one parameter with connection string");
+                return;
+            }
+
+            var connectionString = args[0];
+
+            Console.WriteLine("Starting Ado.NET connection test...");
+            TestConnectionString(connectionString);
+
+            Console.WriteLine("Starting S2 connection test...");
+            S2TestConnectionString(connectionString);
+
+            Console.WriteLine("All tests complete.");
+        }
+
+        public static void TestConnectionString(string connectionString)
+        {
+
+            using SqlConnection conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Ado.NET connection test passed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ado.NET connection test failed with message: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void S2TestConnectionString(string connectionString)
+        {
+            var container = new DataContainer()
+            {
+                ConnectionString = connectionString
+            };
+            var supply = new SqlServerSupplyCollector();
+
+            try
+            {
+                var testResult = supply.TestConnection(container);
+                Console.WriteLine("S2 connection test passed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("S2 connection test failed with message: " + ex.Message);
+            }
+        }
+    }
+}
